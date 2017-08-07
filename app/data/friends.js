5bf77f3701ed3c341313315e1c8b3fad69525008
gtbmed <jkbarson@gmail.com>
2017-08-06 23:02:02 -0400

    need a shorter commute.  3 hour round trip is killing me

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-var friend = [
+var friends = [
   {
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -14,5 +14,70 @@ var friend = [
         4,
         1
       ]
+  },
+  {
+    "name":"Bruce",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        1,
+        3,
+        4,
+        2,
+        5,
+        2,
+        3,
+        1,
+        3
+      ]
+  },
+  {
+    "name":"Cheryl",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        2,
+        1,
+        2,
+        3,
+        4,
+        3,
+        4,
+        4,
+        5
+      ]
+  },
+  {
+    "name":"Erika",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Otto Von Bismarck",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+      ]
   }
 ]
+module.exports = friends;
