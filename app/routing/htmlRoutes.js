aed669bf6bc8a57ba8194a6b7049407a84585f8c
John Barson <John@NewAdventure.(none)>
2017-08-10 18:33:09 -0400

    typo fix again

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,11 @@ const path = require('path');
 //HTML Routing
 module.exports = function(app) {
   // Direct to survey page
-  app.get("/survey", function (res,req) {
+  app.get("/survey", function (req,res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"))
   });
   // Default to Homepage
-  app.use(function (res, req) {
+  app.use(function (req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 };
